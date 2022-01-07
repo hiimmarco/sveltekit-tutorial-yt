9ce6d44e3ed7e72de0248ffc8b11e8e6a1fb594b
Marco Bogner <marcobogner@gmail.com>
2022-01-07 15:00:45 +0100

    Change config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
+		adapter: adapter(),
+
+		// ... truncated ...
+
+		target: '#svelte'
 	}
 };
+
+export default config;
