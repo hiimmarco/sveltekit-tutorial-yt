cc8436c4fecbd45c8edc3b6a1d847ff708dca9d1
Marco Bogner <marcobogner@gmail.com>
2022-01-07 16:04:59 +0100

    remove unnecessary comments

--- src/routes/p/[slug].svelte
+++ src/routes/p/[slug].svelte
@@ -7,17 +7,6 @@
 	}
 </script>
 
-<!-- <script>
-	export let singleProduct;
-	let product = $singleProduct;
-	let productImage = product.images.edges[0].node.url;
-</script> -->
-
-<!-- <main>
-	<h1>Single Product</h1>
-	<img src={productImage} alt="Main" />
-</main> -->
-
 <h1>Product: {$singleProduct.title}</h1>
 <p>Desription: {$singleProduct.description}</p>
 <p>Price: {formatPrice($singleProduct.priceRange.minVariantPrice.amount)}</p>
