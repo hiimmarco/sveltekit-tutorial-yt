1aee4d841cb8b6f37311b418e321a21bfa711daa
Marco Bogner <marcobogner@gmail.com>
2022-01-19 12:15:55 +0100

    Add tailwind config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
 	plugins: {
-		autoprefixer: {},
-		tailwindcss: {}
+		'postcss-import': {},
+		'postcss-nesting': {},
+		tailwindcss: {},
+		autoprefixer: {}
 	}
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,20 @@
 <script>
+	import '../styles/tailwind.css';
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">Welcome to markta</h1>
 <a href="/products">
 	<button class="bg-red-500 text-center">Go to products</button>
 </a>
+
+<div class="flex flex-col justify-center align-middle p-4 mb-12">
+	<h2 class="text-4xl text-center text-purple-700 font-light">Welcome to SvelteKit</h2>
+
+	<p class="text-center mb-4 mt-2">
+		Visit
+		<a class="text-purple-400 hover:text-pink-700 hover:underline" href="https://kit.svelte.dev">
+			kit.svelte.dev
+		</a>
+		to read the documentation.
+	</p>
+</div>
